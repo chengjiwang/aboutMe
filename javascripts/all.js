d0c121c4efa1b6371b41010da4f2852a1732ce24
chengjiwang <chengji3728@gmail.com>
2018-09-06 15:04:47 +0800

    修改project和自介和一些css

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,5 +1,19 @@
 
 $( document ).ready(function() {
+  function upShowToggle() {
+    var scrollValue = $(this).scrollTop(),
+      windowHeight = $(window).height()*0.5;
+    if (window.innerWidth > 767){
+      if (scrollValue > windowHeight) {
+        $('#up').fadeIn();
+      } else {
+        $('#up').fadeOut();
+      }
+    }
+  }
+  $(window).scroll(function(){
+      upShowToggle();
+  });
   $('#js-show-menu, #js-menu a').on('click',  function(e){
       e.preventDefault();
       $('.header').toggleClass('menu-show');
@@ -9,5 +23,8 @@ $( document ).ready(function() {
       scrollTop: $(this.hash).offset().top - 50 //scrollTop 是scroll的垂直位置
     }, 1000);
     return false;
+  });
+  $('#up').click(function(){
+      $('html,body').animate({scrollTop:0}, 1000);
   });
 });
